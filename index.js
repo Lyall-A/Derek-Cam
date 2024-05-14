7719a7be555f02b15bb651f3e7b00c82c99aeb64
Lyall <lyall.yeet@gmail.com>
2024-05-14 17:49:02 +0100

    test

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+// TODO: get smart plug state from octoprint api
+// TODO: create images
+
 const childProcess = require("child_process");
+const fs = require("fs");
 const App = require("./utils/HTTP/App");
 
 const config = require("./config.json");
+const secret = JSON.parse(fs.readFileSync("./.secret", "utf-8"));
+
+const offImage = config.offImagePath ? fs.readFileSync(config.offImagePath) : null;
+const defaultImage = config.defaultImagePath ? fs.readFileSync(config.defaultImagePath) : null;
 
 const clients = [];
+
 let running = false;
+let off = false;
 
 (function startStream() {
     const ffmpegArgs = [
@@ -29,7 +39,9 @@ let running = false;
                 client.write(`--stream\r\n`);
                 client.write(`Content-Type: image/jpeg\r\n\r\n`);
             }
+
             client.write(data);
+
             if (data[data.length - 2] == 0xFF && data[data.length - 1] == 0xD9) {
                 client.write("\r\n\r\n");
             }
@@ -61,10 +73,22 @@ app.get("/stream", (req, res, next) => {
     req.on("close", () => {
         delete clients[clientIndex-1];
     });
+
+    if (off && offImage) {
+        res.write(`--stream\r\n`);
+        res.write(`Content-Type: image/jpeg\r\n\r\n`);
+        res.write(offImage);
+        res.write("\r\n\r\n");
+    } else if (!off && !running && defaultImage) {
+        res.write(`--stream\r\n`);
+        res.write(`Content-Type: image/jpeg\r\n\r\n`);
+        res.write(defaultImage);
+        res.write("\r\n\r\n");
+    }
 });
 
 app.use((req, res) => {
     res.writeHead(404).end();
 });
 
-app.listen(config.port, () => console.log(`Listening at :${config.port}`))
+app.listen(config.port, () => console.log(`Listening at :${config.port}`));
