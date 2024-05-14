ac2ea3ae2cad13d1fc82b1fbe3065c16b09c0502
Lyall <lyall.yeet@gmail.com>
2024-05-14 17:22:33 +0100

    test

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let running = false;
             if (data[data.length - 2] == 0xFF && data[data.length - 1] == 0xD9) {
                 client.write("\r\n\r\n");
             }
-            client.end();
+            // client.end();
         });
     });
 
