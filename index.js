693689a6fa96617ad9c5c93e4219c8d4121ee885
Lyall <lyall.yeet@gmail.com>
2024-05-14 16:01:39 +0100

    ugh

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const childProcess = require("child_process");
-const fs = require("fs");
+const App = require("./utils/HTTP/App");
 
 const config = require("./config.json");
-const streams = require("./streams.json");
 
-streams.forEach((stream, index) => {
-    streams[index].clients = [ ];
-    startStream(stream, index);
-});
+const clients = [];
+let running = false;
 
-function startStream(stream, index) {
+(function startStream() {
     const ffmpegArgs = [
         "-i",
-        stream.path,
+        config.path,
         ...(config.ffmpegArgs?.split(" ") || ""),
         "-f",
         "mjpeg",
         "-"];
-
+        
+    console.log(`Starting FFmpeg instance!`);
     const ffmpegInstance = childProcess.spawn(config.ffmpegPath, ffmpegArgs);
 
-    streams[index].ffmpegArgs = ffmpegArgs;
-    streams[index].ffmpegInstance = ffmpegInstance;
-
     ffmpegInstance.stderr.on("data", data => {
-        streams[index].clients.forEach(client => {
+        running = true;
+        clients.forEach(client => {
             client.write(data); // TODO: multipart
         });
     });
-}
+
+    ffmpegInstance.on("close", () => {
+        running = false;
+        console.log(`FFmpeg instance closed!`);
+        setTimeout(() => startStream(), config.retryDelay);
+    });
+})();
+
+const app = new App();
+
+app.get("/", (req, res) => {
+    // TODO: show printer stats
+});
+
+app.get("/stream", (req, res, next) => {
+    const clientIndex = clients.push(res);
+    // TODO: show cam
+
+    req.on("close", () => {
+        delete clients[clientIndex-1];
+    });
+});
+
+app.get("*", (req, res) => {
+    res.writeHead(404).end();
+});
+
+app.listen(config.port, () => console.log(`Listening at :${config.port}`))
