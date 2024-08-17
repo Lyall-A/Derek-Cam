bd34775902043c045d0b0afa2348545a89f3cb59
Lyall <lyall.yeet@gmail.com>
2024-08-17 20:04:44 +0100

    bun pls

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ const http = require("http");
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-    req.on("close", () => console.log("closed")); // doesnt fire with bun...
+    console.log("opened");
+    req.on("close", () => console.log("closed")); // doesnt fire with bun
     res.end("hi");
 });
 
